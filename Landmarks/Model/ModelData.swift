c6fe3ca55dd7a47a87e0b56f51579dd6c6a14aa3
firstChairCoder <md1qq7@gmail.com>
2024-10-09 12:00:53 +0100

    chore: section 21 - adopt the model object in your views

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -7,9 +7,8 @@
 
 import Foundation
 
-@Observable
-class ModelData {
-    var landmarks: [Landmark] = load("landmarkData.json")
+class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
 }
 
 
