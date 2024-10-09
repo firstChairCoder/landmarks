f67921902ef5c10c7ab410b6d61bda20980b8c26
firstChairCoder <md1qq7@gmail.com>
2024-10-09 09:34:48 +0100

    chore: section 15 - pass data into child views

--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -31,10 +31,18 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
+    var coordinate: CLLocationCoordinate2D
+    
+    @State private var region: MKCoordinateRegion
+        
+        // Custom initializer
+        init(coordinate: CLLocationCoordinate2D) {
+            self.coordinate = coordinate
+            self._region = State(initialValue: MKCoordinateRegion(
+                center: coordinate,
+                span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+            ))
+        }
 
     var body: some View {
         Map(coordinateRegion: $region)
@@ -43,6 +51,6 @@ struct MapView: View {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
     }
 }
