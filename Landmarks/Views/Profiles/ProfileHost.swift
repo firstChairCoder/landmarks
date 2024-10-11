6d594a6d95579f8ec87d3755fb9ab916424a1964
firstChairCoder <md1qq7@gmail.com>
2024-10-11 15:14:23 +0100

    chore: section 37 - display a user profile

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by macbook on 11/10/2024.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
