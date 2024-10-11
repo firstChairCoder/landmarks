d04c16442d0966f08266a912129d613e85b480e5
firstChairCoder <md1qq7@gmail.com>
2024-10-11 12:23:10 +0100

    chore: section 28 - add animations to individual views

--- Landmarks/Views/Hikes/HikeDetail.swift
+++ Landmarks/Views/Hikes/HikeDetail.swift
@@ -32,7 +32,7 @@ struct HikeDetail: View {
                             .foregroundStyle(value.1 == dataToShow
                                 ? .gray
                                 : .accentColor)
-                            .animation(nil) //deprecated
+                            .animation(nil, value: dataToShow)//deprecated
                     }
                 }
             }
